15756b451f6342279b2e2ca279aa34b29945460e
Romain Guy <romainguy@curious-creature.com>
2026-07-01 22:42:47 -0700

    Fix padding in the last demo page

--- vibrance-compose-demo/src/main/kotlin/dev/romainguy/vibrance/demo/BrushGradients.kt
+++ vibrance-compose-demo/src/main/kotlin/dev/romainguy/vibrance/demo/BrushGradients.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -41,7 +42,8 @@ fun BrushGradients(modifier: Modifier) {
         Spacer(Modifier.height(16.dp))
 
         Column(Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
+            .weight(1.0f)
             .background(Color.Black)
             .padding(16.dp)
         ) {
@@ -88,5 +90,7 @@ fun BrushGradients(modifier: Modifier) {
                 )
             }
         }
+
+        Spacer(Modifier.height(24.dp))
     }
 }
